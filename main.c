d8c363feb32457c2d7a83ad06c0e7247de0f1931
pablito <3387@holbertonschool.com>
2021-12-02 12:39:21 -0500

    I added the main function of shell

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+/*	int *argc1 = &argc;*/
+	prompt();
+
+	return (EXIT_SUCCESS);
+
+}
