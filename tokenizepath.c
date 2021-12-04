ae9cdac55ef8c470b600e7e3f0845c8cdc6fd5cf
pablito <3387@holbertonschool.com>
2021-12-03 21:59:36 -0500

    I added a function that tokenize the path obtains with get_env

---
+++ tokenizepath.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stddef.h>
+
+char **tokenize_path(char *road_path)
+{
+	char **tokenize_path = NULL;
+	size_t i = 0;
+
+	tokenize_path = malloc(sizeof(char*) * 1024);
+	tokenize_path[0] = strtok(road_path, ":");
+
+	while (tokenize_path[i])
+	{
+		i++;
+		tokenize_path[i] = strtok(NULL, ":");
+	}
+	printf("%s\n", tokenize_path);
+	return (tokenize_path);
+}
