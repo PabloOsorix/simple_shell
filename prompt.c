3bf9258a75fd547e2da1965ec5e37cbfa530068a
pablito <3387@holbertonschool.com>
2021-12-07 13:25:15 -0500

    I added changes of the prompt

--- prompt.c
+++ prompt.c
@@ -4,26 +4,36 @@
  *prompt - Wait for a command
  *
  **/
-void prompt(void)
+void prompt(int argc, char **argv)
 {
-	char *prompt = "$ ", *input = NULL, *road_path;
-        char **input_tokenize, **path_tokenize;
-	int i = 0;
-
+	char *prompt = "$ ", *input = NULL, *road_path = NULL, *path_copy = NULL;
+        char **input_tokenize = NULL, **path_tokenize = NULL;
+	(void)argc;
 	while(1)
 	{
-		write(STDOUT_FILENO, prompt, _strlen(prompt));
+		if(isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, _strlen(prompt));
 
 		input = read_input();
 		input_tokenize = tokenizeinput(input);
+		if (input_tokenize[0] == NULL)
+		{
+			free(input_tokenize);
+			free(input);
+			continue;
+		}
 		road_path = get_env("PATH");
-		path_tokenize = tokenize_path(road_path);
+		path_copy = _strdup(road_path);
+		path_tokenize = tokenize_path(path_copy);
 
 		if (get_stat(path_tokenize, input_tokenize) == -1)
 		{
-			perror("No sirvió pa una mondá");
+			_perror(argv[0], input_tokenize[0]);
 		}
 
-
+		free(path_tokenize);
+		free(path_copy);
+		free(input_tokenize);
+		free(input);
 	}
 }
