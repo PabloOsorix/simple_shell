6f1a09883d58713d4fcc3ff4115d50e826ea88a0
pablito <3387@holbertonschool.com>
2021-12-07 13:26:58 -0500

    I update our read_input

--- read_input.c
+++ read_input.c
@@ -9,10 +9,9 @@ char *read_input(void)
 {
 	char *input = NULL;
 	size_t bufsize = 0;
-	int chars_read = 0;
 
-	chars_read = getline(&input, &bufsize, stdin);
-	if (chars_read == -1)
+
+	if (getline(&input, &bufsize, stdin) == -1)
 	{
 		free(input);
 		exit(EXIT_SUCCESS);
