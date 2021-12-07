f707d6d48b20e230d3a90ccb79657cb5b198163c
pablito <3387@holbertonschool.com>
2021-12-07 13:26:17 -0500

    I added changes to the header file.

--- shell.h
+++ shell.h
@@ -1,29 +1,33 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define _GNU_SOURCE
 
-extern char **environ;
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 
 #define CMD_DELIM " \t"
 
+extern char **environ;
+
 void prompt(int argc, char **argv);
+
 char *read_input(void);
-int _strlen(char *);
 char *get_cmd(char *);
-int check_cmd(char**);
 char *get_env(char *);
+int get_stat(char **path, char **input);
+
+int _strlen(char *);
 int _strcmp(char *s1, char *s2, int n);
+char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
+
 char **tokenize_path(char *road_path);
 char **tokenizeinput (char *input);
-int get_stat(char **path, char **input);
+
 void _perror(char *shell_name, char *command_name);
 #endif
