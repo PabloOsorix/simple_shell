8d95c5e9dbe8a29df2bfcd0ab4ae38b1aac14c1c
pablito <3387@holbertonschool.com>
2021-12-07 13:29:01 -0500

    i update tojenizeinput

--- tokenizeinput.c
+++ tokenizeinput.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * tokenizationinput - This function takes the command given by
+ * the user and split it into tokens.
+ * @input: Command given by the user.
+ * Return: A array with pointers to the diferent tokens.
+ */
 #define TOK_BUF 64
-#define TOK_DELIM "\t\r\n\a"
+#define TOK_DELIM " \t\r\n\a"
 
 char **tokenizeinput (char *input)
 {
 	int bufsize = TOK_BUF, i = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
+	char **tokens = malloc(bufsize * sizeof(char*) * 1024);
 	char *strtok_delim;
 
 	if(!tokens)
@@ -20,13 +26,13 @@ char **tokenizeinput (char *input)
 		tokens[i] = strtok_delim;
 		i++;
 
-		if(i >= bufsize)
+		/*if(i >= bufsize)
 		{
 			bufsize += TOK_BUF;
 			tokens = realloc(tokens, bufsize * sizeof(char*));
 			if(!tokens)
 				exit(EXIT_FAILURE);
-		}
+				}*/
 		strtok_delim = strtok(NULL, TOK_DELIM);
 	}
 	tokens[i] = NULL;
