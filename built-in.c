0cb82311af3bef638a1812f7764c2bb0003cc126
pablito <3387@holbertonschool.com>
2021-12-08 22:29:19 -0500

    I added the env buil-in

--- built-in.c
+++ built-in.c
@@ -23,3 +23,25 @@ void (*selectfunction(char **args))(char **args, char *line)
 	}
 	return (NULL);
 }
+
+
+#include "shell.h"
+/**
+ * _env - Displays the file containing all global variables
+ * @av: The arguments
+ * @line: The line
+ * Return: Always 0 Success
+ */
+int _env(char **av, int line)
+{
+	int i = 0;
+	(void)av;
+	(void)line;
+
+	while (environ[i])
+	{
+		d_printf(STDOUT_FILENO, "%s\n", environ[i]);
+		i++;
+	}
+	return (0);
+}
